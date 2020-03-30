2104524071988c4dc23e8c56d694a5a5357e38ef
longhai <longhai@applh.com>
2020-03-30 17:09:59 +0200

    ajout css dev1

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
     <h1>COUCOU DEV2</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt delectus quae tenetur? Aperiam at eum ratione enim quaerat, porro quo placeat amet molestias sunt qui ipsa expedita. Ea, facere eaque?</p>
     <h5>DEV2 VA AUSSI RAJOUTER DU CODE EN MEME TEMPS</h5>   
-    <h4>DEV1 RAJOUTE DU CODE</h4>   
+    <h4>DEV1 RAJOUTE DU CODE</h4>
+    
+    <section class="services">
+        <h5>DEV1</h5>
+    </section>
+
+    <section class="produits">
+        <h5>DEV2</h5>
+    </section>
+
 </body>
 </html>
