838356c10ce1b818a2e3164287ef57f1beb428c5
longhai <longhai@applh.com>
2020-03-30 17:03:15 +0200

    ajout fichier style-dev2.css

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-dev2.css">
+
 </head>
 <body>
     <h1>COUCOU DEV2</h1>
